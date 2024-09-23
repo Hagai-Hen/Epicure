3932f5a4d6f40d2fd43e9aa9d76337f9a83e4f92
Hagai Hen <hagaih@moveo.co.il>
2024-09-23 14:58:50 +0300

    finish Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Navbar } from "./Component/Navbar/Navbar"
 import { TopBanner } from "./Component/TopBanner/TopBanner"
+import CardPic from './assets/pictures/card_pic.svg';
+import { Card } from "./Component/Card/Card";
 
 function App() {
 
@@ -7,6 +9,9 @@ function App() {
     <>
       <Navbar />
       <TopBanner />
+      <div style={{width: '245px'}}>
+      <Card pic={CardPic} title="Claro" desc="Ran Shmueli" rate={3}/>
+      </div>
     </>
   )
 }
---
+++ src/Component/Card/Card.tsx
@@ -0,0 +1,35 @@
+import "./styles.scss";
+import filledStar from "../../assets/icons/filled_star.svg";
+import emptyStar from "../../assets/icons/empty_star.svg";
+
+interface CardProps {
+  pic: string;
+  title: string;
+  desc?: string;
+  rate?: number;
+  price?: string;
+}
+
+export const Card = ({ pic, title, desc, rate = 0, price }: CardProps) => {
+  const stars = [];
+
+  for (let i = 0; i < 5; i++) {
+    if (i < rate) {
+      stars.push(<img key={i} src={filledStar} alt="Filled star" />);
+    } else {
+      stars.push(<img key={i} src={emptyStar} alt="Empty star" />);
+    }
+  }
+
+  return (
+    <div className="card_container">
+      <img src={pic} className="pic"/>
+      <section className="text_container">
+        <h1>{title}</h1>
+        <h3>{desc}</h3>
+        <div className="rating">{stars}</div>
+        {price && <p className="price">{price}</p>}
+      </section>
+    </div>
+  );
+};

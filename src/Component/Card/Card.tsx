f806ca02aa4e9ba6397a4825c0230b4679408f2f
Hagai Hen <hagaih@moveo.co.il>
2024-09-24 12:16:43 +0300

    generic Card component

--- src/Component/Card/Card.tsx
+++ src/Component/Card/Card.tsx
@@ -1,50 +1,10 @@
 import "./styles.scss";
-import filledStar from "../../assets/icons/filled_star.svg";
-import emptyStar from "../../assets/icons/empty_star.svg";
-import { CardInterface } from "../../resources/interfaces";
-import nisIcon from "../../assets/icons/nis.svg";
+import { ReactNode } from "react";
 
-export const Card = ({
-  pic,
-  title,
-  desc,
-  rate,
-  price,
-  type,
-}: CardInterface) => {
-  const stars = [];
+interface CardProps {
+  children: ReactNode; // Define children as ReactNode
+}
 
-  if (rate) {
-    for (let i = 0; i < 5; i++) {
-      if (i < rate) {
-        stars.push(<img key={i} src={filledStar} alt="Filled star" />);
-      } else {
-        stars.push(<img key={i} src={emptyStar} alt="Empty star" />);
-      }
-    }
-  }
-
-  return (
-    <div className="card_container">
-      <img src={pic} className="pic" />
-      <section className="text_container">
-        <h1>{title}</h1>
-        {type && <img src={type} className="type_icon" />}
-        <section className="desc_container">
-          <h3>{desc}</h3>
-          <div className="rating">{stars}</div>
-          {price && (
-            <div className="price_container">
-              <span className="line"></span>
-              <span className="price">
-                <img src={nisIcon} alt="NIS Icon" className="nis-icon" />{" "}
-                {price}
-              </span>
-              <span className="line"></span>
-            </div>
-          )}
-        </section>
-      </section>
-    </div>
-  );
+export const Card = ({ children }: CardProps) => {
+  return <div className="card_container">{children}</div>;
 };

fe47dc08053dfe1a70fe9f62357d0d9c9a9f49ad
Hagai Hen <hagaih@moveo.co.il>
2024-09-24 16:52:22 +0300

    add fade using awesome reveal

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import { Navbar } from "./Component/Navbar/Navbar";
 import { TopBanner } from "./Component/TopBanner/TopBanner";
 import { Slider } from "./Component/Slider/Slider";
 import { POPULAR_REST, DISHES } from "./resources/content";
+import { Fade } from "react-awesome-reveal";
+import { ChefBanner } from "./Component/ChefBanner/ChefBanner";
+import { About } from "./Component/About/About";
+import { Footer } from "./Component/Footer/Footer";
+import { KeyBanner } from "./Component/KeyBanner/KeyBanner";
 
 function App() {
   return (
     <>
       <Navbar />
       <TopBanner />
-      <Slider title={POPULAR_REST.TITLE} cards={POPULAR_REST.DATA} />
-      <Slider title={DISHES.TITLE} cards={DISHES.DATA} />
+      <Fade delay={200}>
+        <Slider title={POPULAR_REST.TITLE} cards={POPULAR_REST.DATA} />
+        <Slider title={DISHES.TITLE} cards={DISHES.DATA} />
+        <KeyBanner />
+        <ChefBanner />
+        <About />
+        <Footer />
+      </Fade>
     </>
   );
 }

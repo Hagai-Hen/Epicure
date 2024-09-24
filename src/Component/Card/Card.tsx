a1e472fff7919cbac7a55891893b27602f831a0d
Hagai Hen <hagaih@moveo.co.il>
2024-09-24 09:53:22 +0300

    fixed: consistent size of cards

--- src/Component/Card/Card.tsx
+++ src/Component/Card/Card.tsx
@@ -4,7 +4,14 @@ import emptyStar from "../../assets/icons/empty_star.svg";
 import { CardInterface } from "../../resources/interfaces";
 import nisIcon from "../../assets/icons/nis.svg";
 
-export const Card = ({ pic, title, desc, rate, price, type }: CardInterface) => {
+export const Card = ({
+  pic,
+  title,
+  desc,
+  rate,
+  price,
+  type,
+}: CardInterface) => {
   const stars = [];
 
   if (rate) {
@@ -22,18 +29,21 @@ export const Card = ({ pic, title, desc, rate, price, type }: CardInterface) => 
       <img src={pic} className="pic" />
       <section className="text_container">
         <h1>{title}</h1>
-        {type && <img src={type} className="type_icon"/>}
-        <h3>{desc}</h3>
-        <div className="rating">{stars}</div>
-        {price && (
-          <div className="price_container">
-            <span className="line"></span>
-            <span className="price">
-              <img src={nisIcon} alt="NIS Icon" className="nis-icon" /> {price}
-            </span>
-            <span className="line"></span>
-          </div>
-        )}
+        {type && <img src={type} className="type_icon" />}
+        <section className="desc_container">
+          <h3>{desc}</h3>
+          <div className="rating">{stars}</div>
+          {price && (
+            <div className="price_container">
+              <span className="line"></span>
+              <span className="price">
+                <img src={nisIcon} alt="NIS Icon" className="nis-icon" />{" "}
+                {price}
+              </span>
+              <span className="line"></span>
+            </div>
+          )}
+        </section>
       </section>
     </div>
   );

f83201ef4d92fa0d320f441c216bf799f7ad153e
Hagai Hen <hagaih@moveo.co.il>
2024-11-19 12:05:28 +0200

    fix resize of the top banner picture

--- src/Component/TopBanner/TopBanner.tsx
+++ src/Component/TopBanner/TopBanner.tsx
@@ -3,31 +3,12 @@ import TopBannerPic from "../../assets/pictures/topbanner.svg";
 import TopBannerPicLarge from "../../assets/pictures/top_banner.svg";
 import { SearchBanner } from "./SearchBanner";
 import { TOP_BANNER } from "../../resources/content";
-import { useState, useEffect } from "react";
 
 export const TopBanner = () => {
-  const [bannerImage, setBannerImage] = useState(TopBannerPic);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 786) {
-        setBannerImage(TopBannerPicLarge);
-      } else {
-        setBannerImage(TopBannerPic);
-      }
-    };
-    handleResize();
-
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
   return (
     <div className="top_banner">
-      <img src={bannerImage} />
+      <img src={TopBannerPicLarge} className="large_pic" />
+      <img src={TopBannerPic} className="small_pic" />
       <SearchBanner description={TOP_BANNER.DESC} />
     </div>
   );
